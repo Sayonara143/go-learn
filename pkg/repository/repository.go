243097cf1978d62c1c4fc967e3df66bf00bed397
Alex Bykov <bikov23alex@gmail.com>
2023-07-13 22:16:23 +0500

    feat(start): базовая структура, middleware, connection db(postgres)

---
+++ pkg/repository/repository.go
@@ -0,0 +1,30 @@
+package repository
+
+import (
+	todo "learn-go-vscode"
+
+	"github.com/jmoiron/sqlx"
+)
+
+type Authorization interface {
+	CreateUser(user todo.User) (int, error)
+	GetUser(username, password string) (todo.User, error)
+}
+
+type TodoList interface {
+}
+
+type TodoItem interface {
+}
+
+type Repository struct {
+	Authorization
+	TodoList
+	TodoItem
+}
+
+func NewRepository(db *sqlx.DB) *Repository {
+	return &Repository{
+		Authorization: NewAuthPostgres(db),
+	}
+}
